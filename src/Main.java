59b8d464ae096e82852005838ade09e2bd8a5a47
daniilusacev <2893318@gmail.com>
2023-05-17 11:50:41 +0300

    task2 done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        /* task1 */
        int clientOS = 0;
        if (clientOS == 0){
            System.out.println("Установите версию приложения для iOS по ссылке");
        } else System.out.println("Установите версию приложения для Android по ссылке");
-
+        /* task2 */
+        int clientDeviceYear = 2015;
+        if (clientOS == 0){
+            if (clientDeviceYear < 2015){
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }else System.out.println("Установите версию приложения для iOS по ссылке");
+        }else {
+            if (clientDeviceYear < 2015){
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }else { System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
     }
 }
