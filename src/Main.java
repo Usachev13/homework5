eb418b1f6ba56632b32e50edd826bd046e352b4e
daniilusacev <2893318@gmail.com>
2023-05-17 12:21:18 +0300

    task5 done

--- src/Main.java
+++ src/Main.java
@@ -36,6 +36,49 @@ public class Main {
             daysForDelivery += 3;
             System.out.println("Потребуется дней: " + daysForDelivery);
         }else System.out.println("Доставка не осуществляется");
+        /* task5 */
+        int monthNumber = 12;
+        switch (monthNumber){
+            case 1:
+                System.out.println("Зима");
+                break;
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+                System.out.println("Весна");
+                break;
+            case 4:
+                System.out.println("Весна");
+                break;
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+                System.out.println("Лето");
+                break;
+            case 7:
+                System.out.println("Лето");
+                break;
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+                System.out.println("Осень");
+                break;
+            case 10:
+                System.out.println("Осень");
+                break;
+            case 11:
+                System.out.println("Осень");
+                break;
+            case 12:
+                System.out.println("Зима");
+                break;
+            default:
+                System.out.println("Нет такого месяца");
+        }
+
     }
 
 }
